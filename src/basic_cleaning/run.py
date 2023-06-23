83aa22d3c591ec4929942cfda60b7d84767bd968
anhtuanluu <atuanbk58@gmail.com>
2023-06-23 16:49:54 +0700

    final

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,8 +27,10 @@ def go(args):
     logger.info('Drop outlier')
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+    # add new fix
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
+    
     logger.info('Save csv')
     df.to_csv("clean_sample.csv", index=False)
 
